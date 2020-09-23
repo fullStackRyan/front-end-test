512a3e9632e62b36e55739bc9d144cae8d337920
Lukasz FEE <lukasz.szczepanski@phrasee.co>
2020-09-23 14:18:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-function App() {
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 14 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+const App = () => {
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src="./images/dhg_whole.png" />
       </header>
-    </div>
+      <main>
+        <Form
+          layout={'vertical'}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+          >
+            <Input
+              size="large"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+          >
+            <Input.Password
+              size="large"
+            />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              disabled={1>0}
+            >
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </main>
+      <footer>
+      </footer>
+    </>
   );
 }
 
